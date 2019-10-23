4130444666b83115bdcdb79f80a1373e82985baa
mahmutsen <senmahmutm@gmail.com>
2019-10-23 17:10:12 +0300

    Inıtıal Project

---
+++ Bang Blocks/Assets/Scripts/Block.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //config params
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
+
+    //cached Referance
+    Level level;
+
+    //state
+    [SerializeField] int timesHit; //only serialized for debugging purposes.(değişimi inspectorde takip etmek...)
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            int spriteIndex = timesHit - 1;
+            if (hitSprites[spriteIndex] != null)
+            {
+                GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+            }
+            else
+            {
+                Debug.LogError("Block sprite is missing from the array " + gameObject.name);
+            }
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        PlayBlockDestroyedSFX();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        TriggerSparklesVFX();
+    }
+
+    private void PlayBlockDestroyedSFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position); //Allows us to create a temporary audio source that does not belong to specific game object.
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+
+}
